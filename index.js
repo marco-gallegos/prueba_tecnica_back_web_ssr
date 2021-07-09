2e265895bc6939c5dbd8c3f5f1e19cf57eeebafb
marco-gallegos <ma_galeza@hotmail.com>
2021-07-09 01:44:39 -0500

    fix(index): cors
    added cors middleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
